ad87ef20a8683c373f92a1bb04278b1890469301
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2021-07-09 11:24:34 +0200

    Added package long description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+from os import path
 from distutils.core import setup
 
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(name='avonage',
-      version='0.1',
+      version='0.1.1',
       description='Asynchronous Vonage / Nexmo client',
       url='https://github.com/adyachok/avonage',
       author='Andras Gyacsok',
@@ -14,4 +20,6 @@ setup(name='avonage',
       install_requires=[
           'aiohttp',
       ],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       zip_safe=False)
